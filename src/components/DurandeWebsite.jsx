0f4b90d7329f577e6b00568a724e5946c7e7bb04
online4302-cyber <online4302@gmail.com>
2026-05-21 15:53:00 +0100

    Update phone number format in DurandeWebsite component

--- src/components/DurandeWebsite.jsx
+++ src/components/DurandeWebsite.jsx
@@ -1141,7 +1141,7 @@ export default function DurandeWebsite() {
                 <div className="mt-10 space-y-3">
                   {[
                     { icon: Mail, label: "info@durande.com" },
-                    { icon: Phone, label: "+44 (0) 20 0010011" },
+                    { icon: Phone, label: "+44 (0) 20 00100111" },
                     { icon: MapPin, label: "85 Station Road, North Harrow,Harrow, HA2 7SW United Kingdom" },
                   ].map((c) => {
                     const Icon = c.icon;
